e1e0988ae3701323a862de5dbb3512194693d847
David Brott <david.brott@outlook.de>
2018-12-04 21:44:19 +0100

    -gitignore is now working

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.akjn.whatsappdirectmessage;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void contactOnWhatsApp(View v)
-    {
-        EditText phoneNumberField = (EditText)findViewById(R.id.inputField);
+    public void contactOnWhatsApp(View v) {
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
         String phoneNumber = phoneNumberField.getText().toString();
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone="+phoneNumber));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
         startActivity(browserIntent);
     }
 }
