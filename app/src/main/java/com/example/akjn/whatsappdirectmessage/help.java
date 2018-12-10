b3ea9791c75abe240b6eb32226afd6064679442d
AMARTYA MONDAL <hayyoulistentome@gmail.com>
2018-12-10 20:24:16 +0530

    Added dark theme for the help activity #68 (#69)
    * Added the multi language version of help activity
    
    * Added dark theme for help activity #68

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/help.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/help.java
@@ -2,11 +2,18 @@ package com.example.akjn.whatsappdirectmessage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class help extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES)
+        {
+            setTheme(R.style.DarkTheme);
+        }
+        else
+            setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
     }
