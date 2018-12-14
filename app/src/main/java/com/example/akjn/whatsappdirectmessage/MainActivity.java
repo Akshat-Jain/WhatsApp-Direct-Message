b0b67f9453e8e35343b45565d5959263c8c32abe
AMARTYA MONDAL <hayyoulistentome@gmail.com>
2018-12-14 20:37:54 +0530

    Requested changes made (#95)
    * Cleaned the app. Removed the spinner
    
    * Finally cleaned

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -41,8 +41,6 @@ import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
-    Locale myLocale;
-    String currentLanguage = "en", currentLang;
     private EditText mPhoneNumber = null;
 
     @Override
@@ -58,63 +56,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentLanguage = getIntent().getStringExtra(currentLang);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Select Language");
-        list.add("English");
-        list.add("Hindi");
-        list.add("Bengali");
-        list.add("French");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        break;
-                    case 1:
-                        setLocale("en");
-                        break;
-                    case 2:
-                        setLocale("hi");
-                        break;
-                    case 3:
-                        setLocale("bng");
-                        break;
-                    case 4:
-                        setLocale("fr");
-                        break;
-
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
 
     }
-
-
-    public void setLocale(String localeName) {
-
-        myLocale = new Locale(localeName);
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-        conf.locale = myLocale;
-        res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(this, MainActivity.class);
-        refresh.putExtra(currentLang, localeName);
-        startActivity(refresh);
-
-    }
-
-
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu)
@@ -166,11 +109,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();
                 return true;
 
-
-            case R.id.aboutus:
-                Intent intents = new Intent(MainActivity.this, AboutUs.class);
-                startActivity(intents);
-                break;
 
             default:
                 return super.onOptionsItemSelected(item);
