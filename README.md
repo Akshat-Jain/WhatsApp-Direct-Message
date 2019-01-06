46218b5f624a66b8da93b17238765abbfa4a499b
Akshat Jain <akjn11@gmail.com>
2019-01-07 00:57:04 +0530

    Fetching commits from NWoC 2018 (#17)
    * COUNTRY CODE AND NEME SPINNER
    
    * Add constraint layout
    
    * Add constraint layout
    
    * Delete Local.properties
    
    Your local configuration
    
    * Restricted the app to portrait mode only.
    
    * Added Icon
    
    * Updated strings.xml file
    
    * added menu at top
    
    * -gitignore is now working
    
    * .gitignore is now working
    
    * no message
    
    * no message
    
    * Change Layout to Coordinator Layout
    
    * no message
    
    * Fixed change app theme (#2)
    
    * Added the multilanguage support#9
    
    * Updated theme as per your requirements
    
    * changed colors according to material design principles
    
    * added background to homescreen
    
    * updated Readme
    
    fixes #36
    Added screenshots of the app.
    
    * synced repo and fixed bugs
    
    * modified country code spinner
    
    * fixed spinner layout
    
    * Update README.md
    
    * Updated Readme
    
    Changed size of screenshots.
    
    * Added apk
    
    * modified apk
    
    * Updated th eicon of the app
    
    * Added the hindi, french, bengali version of phone number hint
    
    * Added a clear Button (#51)
    
    * Added dark theme (#52)
    
    * Added the multilanguage version of menu bar #54 (#55)
    
    * Added the activity for help button #49 (#53)
    
    * updated apk version (#57)
    
    * changed the background of clear button (#59)
    
    * fixed bug in Mainactivity class (#61)
    
    * added multi language support for clear text button (#63)
    
    * Added Dark mode screenshot in README (#65)
    
    * Update README.md
    
    * Added dark mode screenshot
    
    * Added the multi language version of help activity (#67)
    
    * Added dark theme for the help activity #68 (#69)
    
    * Added the multi language version of help activity
    
    * Added dark theme for help activity #68
    
    * Made India as the default country in country checker spinner#70 (#71)
    
    * Added the multi language version of help activity
    
    * Made India default country in the country checker spinner #70
    
    * Fixed the issue of adding the country code also (#79)
    
    * Added the multi language version of help activity
    
    * Added the feature  to save the phone number
    
    * Fixed the issue of not saving the country code
    
    * Added About Us feature in the menu bar#72 (#73)
    
    * Added the multi language version of help activity
    
    * Added the About US feature in menu bar
    
    * Added activity for About US option (#75)
    
    * Added the multi language version of help activity
    
    * Added the About US feature in menu bar
    
    * Added the About Us activity
    
    * Added the feature to save the phone number added (#77)
    
    * Added the multi language version of help activity
    
    * Added the feature  to save the phone number
    
    * Fixed the issue of returning back to light mode after dark mode is selected (#82)
    
    * Added the multi language version of help activity
    
    * Fixed the issue to switch back to light mode
    
    * fixed strings error (#89)
    
    * Requested changes made (#95)
    
    * Cleaned the app. Removed the spinner
    
    * Finally cleaned
    
    * Added rounded button (#101)
    
    * Added Popup Message (#103)
    
    * Fixed some Issues. (#107)
    
    * Made Dashboard Scrollable
    
    * Increased Radius of Buttons
    
    * Removed help Activity
    
    * Changed backgrounds
    
    * Changed Manifest
    
    * Fixed blank input in save contact button (#111)
    
    * Fixed blank input in save contact button
    
    * Created method for Alert Dialog
    
    * Added checkbox in menu to change theme (#114)
    
    * fix: Fix error occurred on sending message if whatsapp is not installed. (#119)
    
    * Made the respective changes and updated the screenshots  (#120)
    
    * MAde the respective changes and updated the screenshots
    
    * UPdated the link for the apk
    
    * Resized the images (#122)
    
    * Resizing the images (#123)

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ A simple app which saves you the effort of saving numbers as contacts if you wis
 
 ### Download the app
 
-You can download by clicking [here](https://github.com/Akshat-Jain/WhatsApp-Direct-Message/raw/master/apk/WhatsApp%20Direct%20Message.apk) :-)
+You can download by clicking [here](https://github.com/NJACKWinterOfCode/WhatsApp-Direct-Message/raw/master/apk/WhatsApp_Direct_Message.apk) :-)
 
 ### Install and contribute
 
@@ -19,3 +19,9 @@ You can download by clicking [here](https://github.com/Akshat-Jain/WhatsApp-Dire
 $ git clone https://github.com/Akshat-Jain/WhatsApp-Direct-Message.git
 ```
 Then open in Android Studio.
+#### Screenshots
+![ss1](https://user-images.githubusercontent.com/43731599/50738265-fb2aef00-11f7-11e9-985f-5df9361d6af9.png)
+![ss2](https://user-images.githubusercontent.com/43731599/50738266-fc5c1c00-11f7-11e9-82ba-6a968f073c3a.png)
+![ss3](https://user-images.githubusercontent.com/43731599/50738274-09790b00-11f8-11e9-8046-9298f8d78d3c.png)
+![ss4](https://user-images.githubusercontent.com/43731599/50738277-1695fa00-11f8-11e9-85f8-95861fba519a.png)
+![ss5](https://user-images.githubusercontent.com/43731599/50738279-201f6200-11f8-11e9-959a-2084ff5404e5.png)
--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -1,25 +1,150 @@
 package com.example.akjn.whatsappdirectmessage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.EditText;
 import android.view.View;
-import android.content.Intent;
 import android.net.Uri;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    private EditText mPhoneNumber = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences sp = getSharedPreferences("Mydata", Context.MODE_PRIVATE);
+        if (sp.getString("theme", "light").equals("light"))
+            setTheme(R.style.AppTheme);
+        else
+            setTheme(R.style.DarkTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void contactOnWhatsApp(View v)
-    {
-        EditText phoneNumberField = (EditText)findViewById(R.id.inputField);
-        String phoneNumber = phoneNumberField.getText().toString();
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone="+phoneNumber));
-        startActivity(browserIntent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    public void contactOnWhatsApp(View v) {
+
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
+        if (phoneNumberField.getText().toString().isEmpty()) {
+            Alert_Dialog_Blank_Input();
+        } else {
+            //  CODE FOR COUNTRY CODE SPINNER
+            CountryCodePicker cpp = (CountryCodePicker) findViewById(R.id.cpp);
+            cpp.registerCarrierNumberEditText(phoneNumberField);
+            String phoneNumber = cpp.getFullNumber();
+
+            boolean installed = appInstalledOrNot("com.whatsapp");
+            if(installed) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
+                startActivity(browserIntent);
+            } else {
+                Toast.makeText(this,"Whatsapp is not installed on your device",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
+
+    //Function to save contact
+    public void SaveContact(View v) {
+
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
+        if (phoneNumberField.getText().toString().isEmpty()) {
+            Alert_Dialog_Blank_Input();
+        } else {
+            Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+            intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+            mPhoneNumber = (EditText) findViewById(R.id.inputField);
+
+            CountryCodePicker cpp = (CountryCodePicker) findViewById(R.id.cpp);
+            String mNo = cpp.getFullNumberWithPlus() + mPhoneNumber.getText();
+            intent.putExtra(ContactsContract.Intents.Insert.PHONE, mNo);
+
+            startActivity(intent);
+        }
+
+    }
+
+
+    //Function to clear numbers
+    public void clearNumber(View view) {
+        EditText hello = (EditText) findViewById(R.id.inputField);
+        hello.setText("");
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences sp = getSharedPreferences("Mydata", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        switch (item.getItemId()) {
+
+            case R.id.dark_mode:
+                //add code here
+                if (!item.isChecked()) {
+                    editor.putString("theme", "dark");
+                    editor.commit();
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                    break;
+                } else {
+                    editor.putString("theme", "light");
+                    editor.commit();
+                    Intent ins = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(ins);
+                    finish();
+                    break;
+                }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void Alert_Dialog_Blank_Input() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Error");
+        builder.setMessage("Please Enter A Number");
+        AlertDialog alert = builder.create();
+        alert.getWindow().setGravity(Gravity.CENTER);
+        alert.show();
     }
 }
+
